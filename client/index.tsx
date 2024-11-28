8d83c959ef038a7a2d44525a05b8402dd8e25041
Mikhail Zuenko <zuenko.michail@gmail.com>
2024-11-28 22:31:47 +0700

    Исправлен обгон запросов, подправлены стили

--- client/index.tsx
+++ client/index.tsx
@@ -1,15 +1,14 @@
 import Good from './good.tsx';
 import styles from './index.scss';
-import { getApi, postApi } from './lib/api.ts';
+import { postApi } from './lib/api.ts';
 import { $append, $clear, $prepend, $remove } from './lib/dom.ts';
 
 const $logo: HTMLHeadingElement = <h1 class={styles.logo}>Маркетплейс</h1>;
-const $search: HTMLInputElement = <input class={styles.search} placeholder="Поиск" autofocus onInput={function(event: KeyboardEvent) {
+const $search: HTMLInputElement = <input class={styles.search} placeholder="Поиск" autofocus onInput={function() {
     history.pushState(null, null, $search.value === '' ? '/' : `?query=${encodeURIComponent($search.value)}`);
     historyUpdated();
 }} />;
-const $numGoods: Text = <></>;
-const $description: HTMLElement[] = <><div class={styles.shops}><a class={styles.ozon} href="https://www.ozon.ru/" target="_blank" /><a class={styles.wildberries} href="https://www.wildberries.ru/" target="_blank" /><a class={styles.other} href="https://bombers.ext.io/" target="_blank" /></div><p class={styles.description}>Маркетплейс - это сервис по поиску и сравнению товаров по интернет-магазинам рунета, предоставляющий возможность сэкономить время по серфингу одинаковых товаров на разных площадках!</p><p class={styles.description}>Преимущества: быстро, удобно, не придется рассчитывать выгоду и скорость доставки.</p></>;
+const $description: HTMLElement[] = <><p class={styles.description}>Маркетплейс - это сервис по поиску и сравнению товаров по интернет-магазинам рунета, предоставляющий возможность сэкономить время по серфингу одинаковых товаров на разных площадках!</p><p class={styles.description}>Преимущества: быстро, удобно, не придется рассчитывать выгоду и скорость доставки.</p><div class={styles.shops}><a class={styles.ozon} href="https://www.ozon.ru/" target="_blank" /><a class={styles.wildberries} href="https://www.wildberries.ru/" target="_blank" /><a class={styles.other} href="https://bombers.ext.io/" target="_blank" /></div></>;
 const $goods: HTMLDivElement = <div />;
 const $main: HTMLDivElement = <div class={styles.main}>{$logo}{$search}{$description}</div>;
 $append(document.body, $main);
@@ -26,6 +25,7 @@ async function historyUpdated() {
     else {
         $remove($logo, $description);
         const { results } = await postApi('search', { query });
+        if ($search.value !== query) return;
         for (const result of results) {
             $append($goods, Good(result.link, result.images[0], result.name, result.price));
         }
--- server/search.ts
+++ server/search.ts
@@ -17,7 +17,7 @@ export function search(
     getPath: (query: string, page: number) => string,
     parse: (response: any) => Promise<Good[]>
 ) {
-    return async function(query: string, pages = 2) {
+    return async function(query: string, pages = 1) {
         const session = new Session(host);
         const results = await Promise.all(Array.from({ length: pages }, async (_, i) => await parse(JSON.parse(await session.get(getPath(encodeURIComponent(query), i + 1))))));
         session.close();
