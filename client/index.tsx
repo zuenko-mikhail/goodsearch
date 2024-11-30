94af3b606f6380ff234eebb21d3c29bb860d94c2
Mikhail Zuenko <zuenko.michail@gmail.com>
2024-12-01 06:51:18 +0700

    Добавлены логотипы

--- client/index.tsx
+++ client/index.tsx
@@ -25,7 +25,7 @@ const $favoriteStar: HTMLButtonElement = <button class={styles.favoriteStar} onC
 }} />;
 
 /** Основной элемент */
-const $main: HTMLDivElement = <div class={styles.main}><h1 class={styles.logo}>Goodsearch</h1>{$search}{$favoriteStar}<div class={styles.shopsContainer}>Поиск по маркетплейсам:<div class={styles.shops}><a class={styles.joom} href="https://www.joom.ru/" target="_blank" /><a class={styles.ozon} href="https://www.ozon.ru/" target="_blank" /><a class={styles.wildberries} href="https://www.wildberries.ru/" target="_blank" /></div></div></div>;
+const $main: HTMLDivElement = <div class={styles.main}><h1 class={styles.logo}><span class={styles.logoTitle}>Goodsearch</span></h1>{$search}{$favoriteStar}<div class={styles.shopsContainer}>Поиск по маркетплейсам:<div class={styles.shops}><a class={styles.joom} href="https://www.joom.ru/" target="_blank" /><a class={styles.ozon} href="https://www.ozon.ru/" target="_blank" /><a class={styles.wildberries} href="https://www.wildberries.ru/" target="_blank" /></div></div></div>;
 $append(document.body, $main);
 
 onChangeParams(function() {
